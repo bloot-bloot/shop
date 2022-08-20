d7a3671f587e5fb8c793105fd185134f8a85c722
илья блутштейн <ilyablutshtein@MacBook-Pro-ila.local>
2022-08-20 15:55:03 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
